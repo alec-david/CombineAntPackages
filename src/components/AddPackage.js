dd7535987a971cb10bfed77431a4d3e31d483992
alec-david <ard72@case.edu>
2017-09-07 13:02:12 -0400

    added combine logic. cleaned up look

--- src/components/AddPackage.js
+++ src/components/AddPackage.js
@@ -1,20 +1,9 @@
 import React, { Component } from 'react';
 import Dropzone from 'react-dropzone';
 
-class AddPackage extends Component {
+import { combineSelectedPackages } from '../CombinePackagesLogic';
 
-  handleChange(e) {
-    let files = e.target.files;
-    let fileList;
-    for (let i = 0; i <= files.length-1; i++) {
-      //let reader = new FileReader();
-      let file = files[i];
-      console.log(file.name);
-      fileList += file.name + '\n';
-    }
-    let fileSpan = document.getElementById('fileList');
-    fileSpan.innerHTML = 'asuh';
-  }
+class AddPackage extends Component {
 
   constructor() {
     super()
@@ -27,11 +16,37 @@ class AddPackage extends Component {
     });
   }
 
+  handleChange() {
+    let files = this.state.files;
+    let fileList;
+    for (let i = 0; i <= files.length-1; i++) {
+      //let reader = new FileReader();
+      let file = files[i];
+      console.log(file.name);
+      fileList += file.name + '\n';
+    }
+    let fileSpan = document.getElementById('fileList');
+    fileSpan.innerHTML = 'asuh';
+  }
+
+  combinePackages() {
+    combineSelectedPackages(this.state.files);
+  }
+
   render() {
+    var dropzoneStyle = {
+      width: 400+'px',
+      height: 200+'px',
+      borderWidth: 2+'px', 
+      borderColor: '#666666', 
+      borderStyle: 'dashed', 
+      borderRadius: 5+'px'
+    }
+
     return (
       <section>
         <div className="dropzone">
-          <Dropzone onDrop={this.onDrop.bind(this)}>
+          <Dropzone onDrop={this.onDrop.bind(this)} style={dropzoneStyle}>
             <p>Try dropping some files here, or click to select files to upload.</p>
           </Dropzone>
         </div>
@@ -39,10 +54,13 @@ class AddPackage extends Component {
           <h2>Selected files</h2>
           <div>
             {
-              this.state.files.map(f => <span key={f.name}>{f.name} - {f.size} bytes</span>)
+              this.state.files.map(f => <span key={f.name}>{f.name} - {f.size} bytes<br/><br/></span>)
             }
           </div>
         </aside>
+        <div>
+          <input type='button' value='Combine Packages' onClick={this.combinePackages.bind(this)}/>
+        </div>
       </section>
     );
   }
