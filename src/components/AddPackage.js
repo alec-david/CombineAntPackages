d6bfdbdfe24d77b6ab7aaef77ae7fff37be12022
alec-david <ard72@case.edu>
2017-09-13 13:08:45 -0400

    the combined package now displays on the screen with the option to download or copy the package to clipboard

--- src/components/AddPackage.js
+++ src/components/AddPackage.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import Dropzone from 'react-dropzone';
+import Clipboard from 'clipboard';
 
 import { combineSelectedPackages } from '../CombinePackagesLogic';
+
+new Clipboard('.btn');
 
 class AddPackage extends Component {
 
@@ -22,11 +25,36 @@ class AddPackage extends Component {
   combinePackages() {
     let combinePromise = combineSelectedPackages(this.state.files);
     combinePromise.then(result => {
-      console.log(result);
       this.setState({
-        packageXML: combinePromise
+        packageXML: result
       });
     });
+  }
+
+  fileDownload(data, filename, mime) {
+    let blob = new Blob([data], {type: mime || 'application/octet-stream'});
+    
+    // IE workaround
+    if (typeof window.navigator.msSaveBlob !== 'undefined') {
+        window.navigator.msSaveBlob(blob, filename);
+    } else {
+      let blobURL = window.URL.createObjectURL(blob);
+      let tempLink = document.createElement('a');
+      tempLink.style.display = 'none';
+      tempLink.href = blobURL;
+      tempLink.setAttribute('download', filename);
+      
+      // Safari thinks _blank anchor are pop ups. We only want to set _blank
+      // target if the browser does not support the HTML5 download attribute.
+      if (typeof tempLink.download === 'undefined') {
+          tempLink.setAttribute('target', '_blank');
+      }
+      
+      document.body.appendChild(tempLink);
+      tempLink.click();
+      document.body.removeChild(tempLink);
+      window.URL.revokeObjectURL(blobURL);
+    }
   }
 
   render() {
@@ -37,27 +65,48 @@ class AddPackage extends Component {
       borderColor: '#666666', 
       borderStyle: 'dashed', 
       borderRadius: 5+'px'
-    }
+    };
+    var formatPackage = {
+      whiteSpace: 'pre-wrap',
+      textAlign: 'left'
+    };
 
-    return (
-      <section>
-        <div className="dropzone">
-          <Dropzone onDrop={this.onDrop.bind(this)} style={dropzoneStyle}>
-            <p>Try dropping some files here, or click to select files to upload.</p>
-          </Dropzone>
-        </div>
-        <aside>
-          <h2>Selected files</h2>
-          <div>
-            {
-              this.state.files.map(f => <span key={f.name}>{f.name} - {f.size} bytes<br/><br/></span>)
-            }
+    let combinedPackage = this.state.packageXML;
+
+    if (!combinedPackage) {
+      return (
+        <section>
+          <div className="dropzone">
+            <Dropzone onDrop={this.onDrop.bind(this)} style={dropzoneStyle}>
+              <p>Try dropping some files here, or click to select files to upload.</p>
+            </Dropzone>
           </div>
-        </aside>
-        <div>
-          <input type='button' value='Combine Packages' onClick={this.combinePackages.bind(this)}/>
+          <aside>
+            <h2>Selected files</h2>
+            <div>
+              {
+                this.state.files.map(f => <span key={f.name}>{f.name} - {f.size} bytes<br/><br/></span>)
+              }
+            </div>
+          </aside>
+          <div>
+            <input type="button" value="Combine Packages" onClick={this.combinePackages.bind(this)}/>
+          </div>
+        </section>
+      );
+    }
+    return (
+      <div>
+        <h3>Combined Package:</h3>
+        <button className="btn" data-clipboard-text={combinedPackage}>
+          Copy package.xml to clipboard
+        </button>
+        <button onClick={() => this.fileDownload(combinedPackage, 'package.xml')}>Download Combined Package</button>
+        <br/><br/>
+        <div style={formatPackage}>
+          <span>{combinedPackage}</span>
         </div>
-      </section>
+      </div>
     );
   }
 }
